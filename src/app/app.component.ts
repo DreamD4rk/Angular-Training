d88385ee0d8d48f7757938fe83e678aee32c1111
DreamD4rk <95450985+DreamD4rk@users.noreply.github.com>
2023-01-04 01:56:27 -0500

    dia 2 operadores y componente nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,72 +11,117 @@ export class AppComponent {
   title = 'Andy';
 
   constructor() {
-    console.log('holex');
+    //operador splice() recibe 2 parámetros
+    //primero creé el array principal 'A', luego otro donde se mostrará el resultado
+    //(en caso quieras mostrar el resultado.. si no solo muestra A.splice(1,1).
+    //modifica el array principal y devuelve lo que está eliminando
+    //usualmente se usa SPLICE con DELETE en las tablas
+    const A = ['a', 'b', 'c', 'd', 'e'];
+    const auxSplice = A.splice(2, 1);
+    console.log('auxSplice:', A, auxSplice);
+    //resultado: auxSplice: ["a", "b", "d", "e"]  ["c"]
 
-    //map() funciona solo con arrays
-    //map() itera: Realizar [cierta acción] varias veces.
-    //map() creo un nuevo array con el resultado
-    const auxMap = [1, 2, 3, 4, 5].map((num) => num * 2);
-    console.log('auxMap:', auxMap);
-    //resultado: auxMap [2, 4, 6, 8, 10]
+    //operador Reduce recibe una funcion flecha
+    //suma y devuelve un array
+    const auxReduce = [1, 2, 3, 4, 5].reduce(
+      (prev, value, index) => prev + value,
+      0
+    );
+    console.log('auxReduce', auxReduce);
+    //resultado: auxReduce: 15
 
-    //forEach() solo recorre el arraay, nada más.
-    const auxForEach = [1, 2, 3, 4, 5].forEach((num) => num * 2);
-    console.log('auxForEach:', auxForEach);
+    //JSON Operadores
+    const auxJSON = {
+      id: 1,
+      nombre: 'juan',
+      apellido: 'rojas',
+    };
 
-    //find() busca el objeto, funciona en JSON's, devuelve objeto entero
-    const auxFind = [1, 2, 3, 4, 5].find((num) => num === 3);
-    console.log('auxFind:', auxFind);
-    //resultado: auxFind: 3
+    //ENTRIES, KEY, VALUES
 
-    //findIndex() solo busca el índice del array
-    const auxFindIndex = [1, 2, 3, 4, 5].findIndex((num) => num === 4);
-    console.log('auxFindIndex:', auxFindIndex);
-    //resultado auxFindIndex: 3
+    //entries, recibe un objeto json y devuelve un array
+    //devuelve el id en un array, el nombre en otro array y el apellido en otro
+    const auxentries = Object.entries(auxJSON);
+    console.log('auxentries', auxentries);
+    //resultado:
+    /** [Array[2], Array[2], Array[2]]
+        0: Array[2]
+          0: "id"
+          1: 1
+        1: Array[2]
+          0: "nombre"
+          1: "juan"
+        2: Array[2]
+          0: "apellido"
+          1: "rojas"      **/
 
-    //indexOf() recibe un arrray o cadena string
-    //usualmente no va funcion flecha
-    //cuado buscamos una palabra entera con indexof() muestra la primera letra de la palabra
-    //si no, muestra -1
-    const auxIndexOf = [1, 2, 3, 4, 5].indexOf(5);
-    console.log('auxIndexOf:', auxIndexOf);
-    //resultado: auxIndexOf: 0
+    //keys, devuelve un array pero sin los valores del objeto x(1,juan, rojas)x
+    const auxKeys = Object.keys(auxJSON);
+    console.log('auxKeys', auxKeys);
+    //resultado:
+    /**
+          ["id", "nombre", "apellido"]
+            0: "id"
+            1: "nombre"
+            2: "apellido"     */
 
-    //filter() devuelve un nuevo array desde el resultado
-    //si no encuentra nada al filtrar devuelve un array vacío
-    // si piden filtrar una info de un objeto va así en la función flecha:
-    //ejemplo: .filter(per.country  === 'Lima')
-    const auxFilter = [1, 2, 3, 4, 5].filter((num) => num % 2 === 0);
-    console.log('auxFilter:', auxFilter);
-    //resultado auxFilter: [2, 4]
+    //value, devuelve solo los valores como array
+    const auxValues = Object.values(auxJSON);
+    console.log('auxValues', auxValues);
 
-    //join() no recibe funcion flecha, solo 1 parámetro. Sirve como un separador
-    const auxJoin = [1, 2, 3, 4, 5].join('-');
-    console.log('auxJoin: ', auxJoin);
-    //resultado auxJoin: 1-2-3-4-5
+    //spread operator, rest operator
+    //spread agrega y/o completa un array. Se ponen !3 puntitos!
+    const b = [1, 2, 3, 4, 5, 6];
+    const c = [...b, 7, 8, 9, 10];
+    console.log('Spread: ', c);
 
-    //split() convierte una cadena en array,
-    const auxSplit = 'hola, mundo, test'.split(',');
-    console.log('auxSplit:', auxSplit);
-    //resultado: auxSplit: ["hola", " mundo", " test"]
+    this.getRest(1, 2, 'holi');
+
+    //creando un objeto nuevo
+    const person = {
+      id: 1,
+      name: 'andy',
+      pais: {
+        idPais: 1,
+        cod: 'Lima',
+      },
+    };
+    //un ejemplo de desestructuracion de un objeto json y/o arrays
+    const { pais /*id*/ } = person;
+    console.log('pais:', pais);
+
+    const data = {
+      0: {
+        id: 1,
+        name: 'juan',
+        age: 24,
+      },
+      1: {
+        id: 2,
+        name: 'ande',
+        age: 8,
+      },
+    };
+
+    //convirtiendo el objeto en un array y sumar las edades
+    //despues del values() y el reduce() se puede poner mas operadores, segun los requerimientos
+    const resp = Object.values(data).reduce(
+      (prev, value) => prev + value.age,
+      0
+    );
+    console.log('resp', resp);
+
+    //tarea: convertir a un array filtrar las personas mayores a 10, mostrar solo los
+    //id's
+    //resultado: [1]
+    //resolucion de la tarea:
+    const resp1 = Object.values(data)
+      .filter((p) => p.age > 10)
+      .map((p) => p.id);
+    console.log('resp1: ', resp1);
   }
-
-  //esto es una función en TS
-  //en TS se debe definir el tipo de dato
-  //si está en PUBLIC se puede llamar desde cualquier lado
-  //si está en PRIVATE solo se puede usar en el mismo componente
-  public getName(name: string): string {
-    return 'hi' + name;
+  //rest operator, recibe N parametros del objeto..
+  getRest(...params: any[]) {
+    console.log('rest: ', params);
   }
-
-  /**funcion flecha, usualmente se pone "const" al comienzo
-  const getName = (name: string) => {return 'hi' + name;}
-  -Cuando la funcion flecha retorna mas de 2 lineas de codigo se abre llaves {}
-  -Cuando retorna solo 1 linea va así:
-  const getName = (name: string) => "hi" + name
-  -Si no tiene parámetros de entreda va así:
-  const getName = () => "hi"
-  -Otro ejemplo de función flecha:
-  const mult = num => num*2
-  **/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MyFirstComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
