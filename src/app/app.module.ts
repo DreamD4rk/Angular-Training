d0d6c483fdc0d1f7ac5843785aa3bcefe4a259d5
DreamD4rk <95450985+DreamD4rk@users.noreply.github.com>
2023-01-05 20:30:02 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyFirstComponent } from './my-first/my-first.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
-  declarations: [AppComponent, MyFirstComponent],
+  declarations: [AppComponent, PersonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
